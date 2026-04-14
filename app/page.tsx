5cac1775eb15a2f6de3c633286ec12d72a93f5ed
Vebjørn Dalan <vmedia@sysdev.no>
2026-04-14 04:07:44 +0200

    test redirect

--- app/page.tsx
+++ app/page.tsx
@@ -79,6 +79,7 @@ export default function TryggBruktbilLanding() {
                 <input type="hidden" name="_captcha" value="false" />
                 <input type="hidden" name="_template" value="table" />
                 <input type="hidden" name="_next" value="https://autovalg.vercel.app/takk" />
+                <input type="hidden" name="_redirect" value="true" />
 
                 <div className="grid gap-5 md:grid-cols-2">
                   <label className="block">
